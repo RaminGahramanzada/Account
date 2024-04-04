62b6ee8c721d4a9b9600d35f0f02c4b4025fd036
ramin <ramin2125@outlook.com>
2024-04-04 11:18:22 +0400

    CustomerController and Customer Service updated

--- src/main/java/com/company/account/service/CustomerService.java
+++ src/main/java/com/company/account/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.company.account.service;
 
+import com.company.account.dto.CustomerDto;
+import com.company.account.dto.converter.CustomerDtoConverter;
 import com.company.account.exception.CustomerNotFoundException;
 import com.company.account.model.Customer;
 import com.company.account.repository.CustomerRepository;
@@ -8,12 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter converter;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter converter) {
         this.customerRepository = customerRepository;
+        this.converter = converter;
     }
     protected Customer findCustomerById(String id){
         return customerRepository.findById(id).orElseThrow(
                 ()-> new CustomerNotFoundException("Customer not found by id: "+id));
+    }
+
+    public CustomerDto getCustomerById(String customerId) {
+        return converter.convertToCustomerDto(findCustomerById(customerId));
     }
 }
